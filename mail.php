029eafbb3a0a7546f7339d0795daa7c700a29b9f
aaron <aaronblac@gmail.com>
2020-04-09 14:52:23 -1000

    final before launch

---
+++ mail.php
@@ -0,0 +1,17 @@
+<?php
+
+if($_POST) {
+  $to = "aaronblac@gmail.com"; // your mail here
+  $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+  $phone = filter_var($_POST["phone"], FILTER_SANITIZE_STRING);
+  $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
+  $body = "Message: $message\nE-mail: $email";
+
+  if(@mail($to, $subject, $body)) {
+    $output = json_encode(array('success' => true));
+    die($output);
+  } else {
+    $output = json_encode(array('success' => false));
+    die($output);
+  }
+}
--- script/script.js
+++ script/script.js
@@ -1,40 +1,56 @@
-(function ($) {
+$(document).ready(function() {
 
-  'use strict';
+  var form = $('#form'),
+    email = $('#email'),
+    phone = $('#phone'),
+    message = $('#message'),
+    info = $('#info'),
+    submit = $("#submit");
 
-  // Contact form.
-  $('.contact-form').each(function () {
-    var $contact_form = $(this);
-    var $contact_button = $contact_form.find('.form-submit');
-    var contact_action = 'submit.php';
-
-    // Display the hidden form.
-    $contact_form.removeClass('hidden');
-
-
-    // Wait for a mouse to move, indicating they are human.
-    $('body').mousemove(function () {
-      // Unlock the form.
-      $contact_form.attr('action', contact_action);
-      $contact_button.attr('disabled', false);
-    });
-
-    // Wait for a touch move event, indicating that they are human.
-    $('body').on('touchmove', function () {
-      // Unlock the form.
-      $contact_form.attr('action', contact_action);
-      $contact_button.attr('disabled', false);
-    });
-
-    // A tab or enter key pressed can also indicate they are human.
-    $('body').keydown(function (e) {
-      if ((e.keyCode === 9) || (e.keyCode === 13)) {
-        // Unlock the form.
-        $contact_form.attr('action', contact_action);
-        $contact_button.attr('disabled', false);
-      }
-    });
-
+  form.on('input', '#email, #phone, #message', function() {
+    $(this).css('border-color', '');
+    info.html('').slideUp();
   });
 
-})(jQuery);
+  submit.on('click', function(e) {
+    e.preventDefault();
+    if (validate()) {
+      $.ajax({
+        type: "POST",
+        url: "mail.php",
+        data: form.serialize(),
+        dataType: "json"
+      }).done(function(data) {
+        if (data.success) {
+          email.val('');
+          phone.val('');
+          message.val('');
+          info.html('Message sent!').css('color', 'green').slideDown();
+        } else {
+          info.html('Could not send mail! Sorry!').css('color', 'red').slideDown();
+        }
+      });
+    }
+  });
+
+  function validate() {
+    var valid = true;
+    var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    if (!regex.test(email.val())) {
+      email.css('border-color', 'red');
+      valid = false;
+    }
+    if ($.trim(phone.val()) === "") {
+      phone.css('border-color', 'red');
+      valid = false;
+    }
+    if ($.trim(message.val()) === "") {
+      message.css('border-color', 'red');
+      valid = false;
+    }
+
+    return valid;
+  }
+
+});
