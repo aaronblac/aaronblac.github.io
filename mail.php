93506b9f400f4814910fd47b6203cda249c799db
aaron <aaronblac@gmail.com>
2020-04-12 09:36:01 -1000

    change mobile proj pics and html about and footer links

--- mail.php
+++ mail.php
@@ -1,11 +1,11 @@
 <?php
 
 if($_POST) {
-  $to = "aaronblac@gmail.com"; // your mail here
+  $to = "aaronblac@gmail.com"; 
   $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
   $phone = filter_var($_POST["phone"], FILTER_SANITIZE_STRING);
   $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
-  $body = "Message: $message\nE-mail: $email";
+  $body = "Message: $message\nE-mail: $email\nPhone: $phone";
 
   if(@mail($to, $subject, $body)) {
     $output = json_encode(array('success' => true));
