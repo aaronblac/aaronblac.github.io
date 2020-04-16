0b78de0c2d342f7ef49952337bd5c87b3835bbc9
aaron <aaronblac@gmail.com>
2020-04-15 16:46:16 -1000

    final 2

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 
 if($_POST) {
-  $to = "aaronblac@gmail.com"; 
+  $to = "aaronblack@aaron-black.com"; 
   $email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
   $phone = filter_var($_POST["phone"], FILTER_SANITIZE_STRING);
   $message = filter_var($_POST["message"], FILTER_SANITIZE_STRING);
